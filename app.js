30ce3697e77ae3b4d1cb9ee4a0a85342734f9318
Rohun Vora <emailrohun@gmail.com>
2020-07-16 15:10:48 -0700

    [add my name]

---
+++ app.js
@@ -0,0 +1,19 @@
+
+function greeting(name, callback) {
+  let sentence = 'Hello, my name is ' + name + ".";
+
+  let otherSentence = callback();
+
+  return sentence + otherSentence
+}
+
+
+
+function anotherGreeting() {
+  let sentence = " I forgot, my last name";
+
+  return sentence;
+}
+
+let myName = 'Rohun';
+console.log(greeting(myName, anotherGreeting))
